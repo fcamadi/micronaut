528f5033332e16b696c6ae56ea036b430392c67a
Fran Camacho <fcamadi@gmail.com>
2021-11-20 22:01:06 +0100

    test: added controller tests

---
+++ mn-quickstart/src/test/java/org/francd/controller/HelloWorldControllerTest.java
@@ -0,0 +1,32 @@
+package org.francd.controller;
+
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@MicronautTest
+class HelloWorldControllerTest {
+
+    @Inject
+    @Client("/")
+    HttpClient httpClient;
+
+    @Test
+    void helloWorldEndpointRespondsWithProperContent() {
+        var response = httpClient.toBlocking().retrieve("/hello/v1");
+        assertEquals("Hello world!",response);
+    }
+
+    @Test
+    void helloWorldEndpointRespondsWithProperContentAndStatusCode() {
+        var response = httpClient.toBlocking().exchange("/hello/v1", String.class);
+        assertEquals("Hello world!",response.body());
+        assertEquals(HttpStatus.OK,response.getStatus());
+    }
+
+}
