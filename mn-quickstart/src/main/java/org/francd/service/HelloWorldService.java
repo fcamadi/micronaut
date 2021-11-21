07cb8a73fcf06044fef9932ea5d86f182b46c7e4
Fran Camacho <fcamadi@gmail.com>
2021-11-21 21:45:38 +0100

    Added logger "org.francd"

--- mn-quickstart/src/main/java/org/francd/service/HelloWorldService.java
+++ mn-quickstart/src/main/java/org/francd/service/HelloWorldService.java
@@ -1,15 +1,20 @@
 package org.francd.service;
 
 import jakarta.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Singleton
 public class HelloWorldService {
+
+    private static Logger log = LoggerFactory.getLogger(HelloWorldService.class);
 
     public String sayHelloWorld() {
         return "Hello world!";
     }
 
     public String sayHelloToName(String name) {
+        log.debug(String.format("Calling sayHelloToName for name: %s",name));
         return String.format("Hello %s",name);
     }
 }
